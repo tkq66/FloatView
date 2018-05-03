2a290c8bde6e6f5d940e92e1d91d543b9271ce82
TK <teekayu8@hotmail.com>
2018-05-04 01:21:25 +0800

    First commit, initializing project

---
+++ setup.sh
@@ -0,0 +1,8 @@
+npm install -g polymer-cli;
+
+# Initialize the polymer project. Select "element" and the appropriate name.
+polymer init;
+
+# Install/update bower
+npm install -g bower;
+bpm update -g bpwer;
